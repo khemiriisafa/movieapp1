673c28ca833c75d3def01386d0036a21834e3c3d
khemiriisafa <safakhemiri8@gmail.com>
2023-10-10 13:28:11 +0100

    first commit

--- src/data.js
+++ src/data.js
@@ -1,65 +1,66 @@
 export const data = [
   {
     imgUrl:
-      "https://filmsvibez.com/cdn/shop/products/Kung-Fu-Panda-Poster-Films-Vibez-1630122362_520x.jpg?v=1630122363",
-    title: "KUNG FU PANDA",
-    cast: "Jack Black, Jackie Chan, Angelina Jolie",
+      "https://cinespia.org/wp-content/uploads/2022/11/mean-girls-poster.jpg",
+    title: "Mean girls",
+    cast: "Lindsay Lohan, Rachel McAdams, Amanda Seyfried",
     rating: 7,
-    desc: `About the movie: To everyone's surprise, including his own, Po, an overweight, clumsy panda, is chosen as protector of the Valley of Peace. His suitability will soon be tested as the valley's arch-enemy is on his way.`,
+    desc: `Cady joins a new public school and befriends Janis and Damian. They warn her to avoid the Plastics, a group led by Regina, but things get worse when she falls in love with Aaron, Regina's ex-lover..`,
   },
   {
-    imgUrl: "https://m.media-amazon.com/images/I/51unGrb-AAL._AC_.jpg",
-    title: "The Matrix",
-    cast: "Keanu Reeves , Laurence Fishburne, Carrie-Anne Moss ",
-    rating: 8,
-    desc: `Thomas Anderson, a computer programmer, is led to fight an underground war against powerful computers who have constructed his entire reality with a system called the Matrix.`,
+    imgUrl: "https://www.themoviedb.org/t/p/original/i6oWPOklGIDKG45tPjcLNH0fRNO.jpg",
+    title: "clueless",
+    cast: "Alicia Silverstone , Paul Rudd, Brittany Murphy ",
+    rating: 7,
+    desc: `Cher is a rich high-school student who is learning to cope with adolescence and its problems. She also helps a new student gain popularity and in the process discovers her own feelings for someon`,
   },
   {
     imgUrl:
-      "https://www.sonypictures.co.uk/sites/unitedkingdom/files/2023-07/SISU%20KEY%20ART_0.JPG",
-    title: "SISU",
-    cast: "Jorma Tommila ,Aksel Hennie, Jack Doolan ",
+      "https://m.media-amazon.com/images/I/51BwEnFkE8L._AC_UF894,1000_QL80_.jpg",
+    title: "The holiday",
+    cast: "Jude Law ,Cameron Diaz, Kate Winslet ",
     rating: 6,
-    desc: `During the last days of World War II, a solitary prospector crosses paths with Nazis on a scorched-earth retreat in northern Finland. When the soldiers decide to steal his gold, they quickly discover they just tangled with no ordinary miner.`,
+    desc: `Two girls from different countries swap homes for the holidays to get away from their relationship issues. However, their lives change unexpectedly when they meet and fall in love with two local guys.`,
   },
   {
     imgUrl:
-      "https://www.movieposters.com/cdn/shop/products/KILLBILL.VOL.1.PW_500x.jpg?v=1574966219",
-    title: "Kill Bill",
-    cast: "Uma Thurman , Lucy Liu , Vivica A. Fox",
-    rating: 8.2,
-    desc: `It stars Uma Thurman as the Bride, a former assassin who swears revenge on a group of assassins (Lucy Liu, Michael Madsen, Daryl Hannah, and Vivica A. Fox) and their leader, Bill (David Carradine), who tried to kill her and her unborn child. Her journey takes her to Tokyo, where she battles the yakuza.`,
+      "https://www.arthipo.com/image/cache/catalog/poster/movie/2060-3084/pfilm2542-confessions-of-a-shopaholic-movie-film-poster-high-resolution-image-778x1100.webp",
+    title: "Confessions of a Shopaholic",
+    cast: "Isla Fisher, Hugh Dancy, Krysten Ritter",
+    rating: 6,
+    desc: `Rebecca Bloomwood is addicted to shopping and accepts a job as a finance columnist to be able to pay a huge debt. Amidst the struggle, she finds herself falling in love with a wealthy entrepreneur.`,
   },
   {
     imgUrl:
-      "https://www.movieposters.com/cdn/shop/products/974b303121fc6607fbe2b21ec7bb2391_9f43f901-611c-4dd4-b6ad-3a5c803f878f_500x.jpg?v=1573584651",
-    title: "Training Day",
-    cast: "Denzel Washington , Ethan Hawke, Antoine Fuqua",
-    rating: 9,
-    desc: `A newly recruited police officer spends his first day as a narcotics officer with a detective who is more than what he appears to be.`,
-  },
-  {
-    imgUrl:
-      "https://i5.walmartimages.com/asr/e243226f-008c-4bf1-9ad5-544c14d5492f_1.6423af1c6310490301827f0ad1e1985c.jpeg",
-    title: "The Good, the Bad and the Ugly",
-    cast: "Clint Eastwood , Eli Wallach, Lee Van Cleef",
+      "https://m.media-amazon.com/images/I/71oTMiWqbiL.jpgttps://www.movieposters.com/cdn/shop/products/974b303121fc6607fbe2b21ec7bb2391_9f43f901-611c-4dd4-b6ad-3a5c803f878f_500x.jpg?v=1573584651",
+    title: "MamaMia",
+    cast: "Meryl Streep , Amanda Seyfried, Pierce Brosnan",
     rating: 7,
-    desc: `During the Civil War, two men, Blondie and Tuco, form an uncomfortable alliance while looking for treasure. They must also outwit Angel Eyes, an outlaw who wants to plunder the riches for himself.`,
+    desc: `As Sophie and her family are busy preparing for her wedding, she secretly invites three men, who were her mother's lovers in the past, with the hope that one of them is her father.`,
   },
   {
     imgUrl:
-      "https://m.media-amazon.com/images/I/71lFzbvRmfL._AC_UF894,1000_QL80_.jpg",
-    title: "The Hangover",
-    cast: "Zach Galifianakis ,Bradley Cooper, Ed Helms",
+      "https://www.themoviedb.org/t/p/original/nxO9loVHMz7QAwNJDBZCfQhZyY9.jpg",
+    title: "Lady Bird",
+    cast: "Saoirse Ronan , Timothée Chalamet, Odeya Rush",
     rating: 8,
-    desc: `The Hangover is a series of American comedy films created by Jon Lucas and Scott Moore, and directed by Todd Phillips. All three films follow the misadventures of a quartet of friends who go on their road trip to attend a bachelor party`,
+    desc: `As senior year comes to an end, Lady Bird must strive to navigate through the ups and downs in her relationships while trying to get into a prestigious college and become popular.
+`,
   },
   {
     imgUrl:
-      "https://m.media-amazon.com/images/M/MV5BMTY0NjI3MzM2Nl5BMl5BanBnXkFtZTcwNDgxNjA5Nw@@._V1_.jpg",
-    title: "The Campaign",
-    cast: "Will Ferrell ,Zach Galifianakis, Jason Sudeikis",
+      "https://m.media-amazon.com/images/M/MV5BMjAyNDEyMzc4Ml5BMl5BanBnXkFtZTgwMjEzNjM0NTM@._V1_.jpg",
+    title: "oscean's 8",
+    cast: "Cate Blanchett ,Sandra Bullock,Anne Hathaway ",
     rating: 7,
-    desc: `Cam Brady and Marty Huggins are political rivals who are running for the same seat and will stop at nothing to outdo each other during their campaigns.`,
+    desc: `Debbie Ocean is released from jail after serving a prison sentence. She assembles a special crew of seven women to steal a diamond necklace, worth 150 million dollars, from the Met Gala.`,
+  },
+  {
+    imgUrl:
+      "https://m.media-amazon.com/images/M/MV5BMjQ3NjM5MTAzN15BMl5BanBnXkFtZTgwODQzMDAwNjM@._V1_.jpg",
+    title: "to all the boys i've loved before",
+    cast: " Noah Centineo, Lana Condor, Anna Cathcart",
+    rating: 7,
+    desc: `The love letters that Lara Jean secretly wrote to her five crushes end up in the mail – and her quiet high school life is turned upside down.`,
   },
 ];
